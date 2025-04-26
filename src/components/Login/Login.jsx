1b43c5293400a0099e7c7dc33464353118696e05
abdulkader33447 <abdulkader33447@gmail.com>
2025-04-27 00:42:56 +0600

    added user login route

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,70 @@
-import React from 'react';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { auth } from "../../firebaseInIt";
+import { Link } from "react-router";
 
 const Login = () => {
+const [success,setSuccess] = useState(false)
+const [errorMessage,setErrorMessage] = useState('')
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+
+
+    // success message
+    setSuccess(false)
+    // reset
+    setErrorMessage('')
+
+
+    // login user
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+        setSuccess(true)
+      })
+      .catch((error) => {
+        console.log(error);
+        setErrorMessage(error.message)
+      });
+  };
+
   return (
-    <div>
-      <h2>this is login</h2>
+    <div className="card bg-base-100 w-full max-w-sm  mx-auto mt-20 shrink-0 shadow-2xl">
+      <div className="card-body">
+        <h1 className="text-2xl">Login now</h1>
+        <form onSubmit={handleLogin} className="fieldset">
+          <label className="label">Email</label>
+          <input
+            type="email"
+            name="email"
+            className="input"
+            placeholder="Email"
+          />
+          <label className="label">Password</label>
+          <input
+            type="password"
+            name="password"
+            className="input"
+            placeholder="Password"
+          />
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </form>
+        <p>New to this website ? Please <Link className="text-blue-500 underline" to="/signUp">Sign Up</Link></p>
+        {
+          errorMessage && <p className="text-red-500">{errorMessage}</p>
+        }
+        {
+         success &&  <p className="text-green-500">User logged in successfully</p>
+        }
+      </div>
     </div>
   );
 };
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,22 +1,43 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { auth } from "../../firebaseInIt";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router";
 
 const SignUp = () => {
   const [errorMessage, setErrorMessage] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleSignUp = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
-    console.log(email, password);
+    const terms = e.target.terms.checked;
+    console.log(email, password,terms);
 
+    setSuccess(false);
     setErrorMessage("");
+
+    if(!terms){
+      setErrorMessage('Please accept our terms and conditions')
+      return ;
+    }
+
+    // password validate
+    const passwordRegExp = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    if (passwordRegExp.test(password) === false) {
+      setErrorMessage(
+        "password must have one lowercase, one uppercase, one digit and 6 characters or longer."
+      );
+      return;
+    }
 
     // create user
     createUserWithEmailAndPassword(auth, email, password)
       .then((newResult) => {
         console.log(newResult);
+        setSuccess(true);
       })
       .catch((error) => {
         console.log(error);
@@ -37,18 +58,37 @@ const SignUp = () => {
             placeholder="Email"
           />
           <label className="label">Password</label>
-          <input
-            type="password"
-            name="password"
-            className="input"
-            placeholder="Password"
-          />
+          <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              name="password"
+              className="input"
+              placeholder="Password"
+            />
+            <button
+              onClick={() => {
+                setShowPassword(!showPassword);
+              }}
+              className="btn btn-xs absolute top-2 right-5"
+            >
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </button>
+          </div>
           <div>
             <a className="link link-hover">Forgot password?</a>
           </div>
+          <label className="label mt-4">
+            <input type="checkbox" name="terms" className="checkbox" />
+            Accept Terms and Conditions
+          </label>
+          <br />
           <button className="btn btn-neutral mt-4">Sign Up</button>
         </form>
+        <p>Already have an account ? please <Link className="underline text-blue-500" to="/login">Login</Link></p>
         {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        {success && (
+          <p className="text-green-500">User has created successfully</p>
+        )}
       </div>
     </div>
   );
